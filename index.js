33df47084da27948e605183940dd8773c1eafb04
Raynald <raynald.gitau@student.moringaschool.com>
2023-03-20 13:55:50 +0300

    finnished assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(){
+    console.log('saturdayFun')
+    return "This Saturday, I want to roller-skate!"
+}
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(){
+    return 'This Monday, I will go to the office.'
+}
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`
+
+}
+function wrapAdjective(wrapper){
+    if (wrapper=='*'||'\||'){
+        return function wrapAdjective(word){
+            return `You are ${wrapper}${word}${wrapper}!`
+        }
+
+    }
+}
